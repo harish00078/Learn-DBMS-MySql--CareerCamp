08aac5f635b46879208b614aa05b59f2efe8672d
harish kumar <harishpathania150@gmail.com>
2023-01-23 12:39:27 +0530

    upgrade

---
+++ harish.session.sql
@@ -0,0 +1,42 @@
+--  here we create our first query with the help of (select) or (from) keyword:
+
+-- SELECT name,major from student;
+
+
+-- here we select all the (columns) from a  particular (table):
+-- by simply using (*):
+
+-- SELECT * FROM student;
+
+
+-- here we only execute the (select) keyword:wihtout any help of the (from) keyword: with the help of (dual-tables):
+-- we can execute queries by simply using (select) keywords:
+-- but we are only able to (execute) the (real-time) queries:
+-- Imp = dual-tables have so many inbuild operation in it:we can use those operations for our (tables):
+
+-- like : here we are adding (two) values:
+
+-- SELECT 1000 + 100;
+
+-- we can also convert the (table) data in the lower-case to upper-case:with the help of (dual-tables) operations:
+
+-- SELECt ucase('coding ninjas');
+
+-- for check date and time we can use:
+
+-- (now) function:
+-- SELECT NOW();
+
+-- we can also use (date-time) dataytypes:
+-- SELECT DATETIME():
+
+-- if we want time in the milliseconds:
+-- for that we can use the (current_timestamp):
+-- means (timestamp) datatypes:
+-- here (5) repesents the (5-digit) value of (milloseconds):
+
+-- SELECT CURRENT_TIMESTAMP(5);
+
+
+
+
